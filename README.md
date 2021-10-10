ab92d58051e6416c3dc16df5c8330ff7c8a0a0ff
3x3y3z3t <recette.lemongrass@yahoo.com.vn>
2021-10-11 00:00:28 +0700

    Add QuickShieldInfo script v1.0

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # SE-IngameScripts
-A collection of my Ingame scripts for Keen Software House's game Space Engineers.
-Note that these are modular scripts, which means you can "merge" them together in one PB.
-All scripts are ready to use.
+A collection of my Ingame scripts for Keen Software House's game Space Engineers.  
+Note that these are modular scripts, which means you can "merge" them together in one PB.  
+All scripts are ready to use, just strap them in a PB and run.  
 
-#Script Listing
+# Script Listing
 | No | Script Name      | Version | Character Count | Description |
 | -- | ---------------- | ------- | --------------- | ----------- |
-|  1 | CannonAmmpPuller |     1.0 |                 |             |
+|  1 | CannonAmmpPuller |     1.0 |                 | This script will pull ammo from inventory and distribute them evenly to all weapons/turrets on current grid. |
+|  2 | QuickShieldInfo  |     1.0 |                 | This script will display shield information on current grid. Require Cython's Energy Shield. |
 
-#Bug reports/Suggestions
-If you found any bugs/issues, you can either head over to the script's Steam page and report there, or open an Issue here.
-If you have any suggestion, you can open a PR.
+# Bug reports/Suggestions
+If you found any bugs/issues, you can either head over to the script's Steam page and report there, or open an Issue here.  
+If you have any suggestion, you can open a PR.  
---
+++ local/QuickShieldInfo/script.cs
@@ -0,0 +1,104 @@
+﻿
+private List<string> s_CythonShieldSubtypeIds = new List<string>()
+{
+    "SmallShipSmallShieldGeneratorBase",
+    "SmallShipMicroShieldGeneratorBase",
+    "LargeShipSmallShieldGeneratorBase",
+    "LargeShipLargeShieldGeneratorBase"
+};
+System.Text.RegularExpressions.Regex s_QSIRegex = new System.Text.RegularExpressions.Regex(":*.\\([^)]*\\)");
+
+int m_QSIUpdateCount = 0;
+
+const string s_QSIVersion = "v1.0";
+//const string s_QSITag = "QSIRequest";
+
+string m_QSIEchoString = "";
+//string m_QSILogString = "";
+System.Text.RegularExpressions.Match m_QSIShieldInfoMatch = null;
+List<IMyTerminalBlock> m_QSIShields = new List<IMyTerminalBlock>();
+IMyTerminalBlock m_QSIMyShield = null;
+//IMyRadioAntenna m_QSIMyAntenna = null;
+
+public void Init_QuickShieldInfo()
+{
+    Runtime.UpdateFrequency = UpdateFrequency.Update10;
+
+    Echo += (string _text) =>
+    {
+        //m_CAPLogString += _text;
+    };
+}
+
+public void Run_QuickShieldInfo()
+{
+    m_QSIEchoString = "";
+    //m_QSILogString = "";
+    m_QSIShields.Clear();
+
+    Sub();
+
+    Echo(m_QSIEchoString);
+}
+
+private void Sub()
+{
+    m_QSIEchoString += "Grid '" + Me.CubeGrid.DisplayName + "': \n";
+    if (m_QSIMyShield == null)
+    {
+        GridTerminalSystem.GetBlocksOfType(m_QSIShields, (IMyTerminalBlock _block) =>
+        {
+            return (Me.IsSameConstructAs(_block) && s_CythonShieldSubtypeIds.Contains(_block.BlockDefinition.SubtypeId));
+        });
+        m_QSIEchoString += "  Shield found: " + m_QSIShields.Count + "\n";
+
+        if (m_QSIShields.Count <= 0)
+        {
+            m_QSIEchoString += "  No Shield Generator found on grid " + Me.CubeGrid.DisplayName + "\n";
+            return;
+        }
+
+        m_QSIMyShield = m_QSIShields[0];
+    }
+
+    if (!m_QSIMyShield.CustomName.EndsWith(":") && !m_QSIMyShield.CustomName.Contains(":"))
+    {
+        m_QSIMyShield.CustomName += ":";
+        return;
+    }
+
+    m_QSIShieldInfoMatch = s_QSIRegex.Match(m_QSIMyShield.CustomName);
+    if (m_QSIShieldInfoMatch.Length <= 0)
+    {
+        m_QSIEchoString += "  Captured block '" + m_QSIMyShield.CustomName + "' is not a Shield Generator.\n";
+        return;
+    }
+
+    string infoStrip = m_QSIShieldInfoMatch.Value;
+    infoStrip = infoStrip.Substring(3, infoStrip.Length - 4);
+    string[] infos = infoStrip.Split('/');
+    long curE = 0;
+    long maxE = 0;
+    if (!long.TryParse(infos[0], out curE) || !long.TryParse(infos[1], out maxE))
+    {
+        m_QSIEchoString += "  Cannot parse shield info '" + infoStrip + "'.\n";
+        return;
+    }
+
+    m_QSIEchoString += string.Format(System.Globalization.CultureInfo.InvariantCulture, "  {0:#,#}/{1:#,#}\n", curE, maxE);
+    
+    m_QSIEchoString += string.Format("\nLast runtime: {0:F}\nCurrent Instruction Count: {1:0}", Runtime.LastRunTimeMs, Runtime.CurrentInstructionCount);
+}
+
+public Program()
+{
+    Init_QuickShieldInfo();
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+    ++m_QSIUpdateCount;
+    Echo("Update Count: " + m_QSIUpdateCount);
+
+    Run_QuickShieldInfo();
+}
